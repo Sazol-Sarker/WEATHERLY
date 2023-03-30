11c29997bc1b3d7b0da1916702331c871db82216
sazol-sarker <sazolsarker1@gmail.com>
2023-03-30 18:27:43 +0600

    added basic site v0.2

---
+++ script.js
@@ -0,0 +1,40 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4a0958b637mshfdc3f5e03188207p1fb4c4jsn2df0c6b38dcb',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeatherReport=(city)=>{
+
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then(response => {
+        
+        
+        console.log(response)
+        cloud_pct.innerHTML = response.cloud_pct
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        max_temp.innerHTML = response.max_temp
+        min_temp.innerHTML = response.min_temp
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+        temp.innerHTML = response.temp
+        wind_degrees.innerHTML = response.wind_degrees
+        wind_speed.innerHTML = response.wind_speed
+        
+    })
+	.catch(err => console.error(err));
+}
+
+SubmitEvent.addEventListener("click",(e)=>
+{
+    e.preventDefault()
+getWeatherReport(city.value)
+
+})
+
+getWeatherReport("Delhi")
